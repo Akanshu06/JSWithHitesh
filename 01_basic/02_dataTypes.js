efe965b2b5698b0ac4d164c1a721ac1d5234145d
Akanshu <122857774+Akanshu06@users.noreply.github.com>
2025-01-04 07:38:48 +0000

    detatpeConvertion

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all code as new version
+
+//alart(3+3);//ReferenceError: alart is not defined     
+//because its nodejs not browser
+
+let name =  "hitesh";
+let age = 6;
+let isLogin = true;
+let state ;
+/*
+number,   2 to the power 53
+string,
+boolean,  true/false
+bigInt,   for integer more than number
+symbal,   uqiuqe
+undefiend, => No valuse asign
+null ,
+*/
+console.log(typeof undefined); //undefiend
+console.log(typeof null); // Object
+console.log( );
+
+
---
+++ 01_basic/03_conversionOpration.js
@@ -0,0 +1,13 @@
+let score = "233";
+let newNumber = Number(score)
+
+console.log(typeof score);//string 
+console.log(typeof newNumber);//number
+
+
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
